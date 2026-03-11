1757a0289a8b87405d6c493e22859cf84952175e
dennisbor026-source <dennisbor026@gmail.com>
2026-03-11 16:57:31 +0300

    C basics assignment

---
+++ c_files/main.c
@@ -0,0 +1,12 @@
+//data types - character,interger,boolean,float,string
+//we need a main function because it serves as the entry point
+#include <stdio.h>
+int main(){
+  
+    printf("text to display %d\n", result);
+
+    return 0;// most operating systems interpret 0 as a succesful run and we are returning 0 because main function is an interger
+
+}
+//ABI
+//API
